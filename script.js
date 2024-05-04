4e390905d15836601e3b81dd4b4bf7f5654b4a6a
Otávio Alencar <oalencardev@gmail.com>
2024-05-04 15:58:39 -0300

    adding rules to the game

--- script.js
+++ script.js
@@ -42,6 +42,8 @@ function renderSquare(){
         let item= document.querySelector(`div[data-item=${i}]`)
         item.innerHTML = square[i]
     }
+
+    checkgame()
 }
 function renderInfo(){
     document.querySelector('.vez').innerHTML = player
@@ -51,4 +53,49 @@ function renderInfo(){
 function togglePlayer(){
     player = (player === 'x') ? 'o' : 'x'
     renderInfo()
+}
+
+function checkgame(){
+    if(checkWinnerFor('x')){
+        warning = 'O "x" venceu'
+        playing = false
+    }else if(checkWinnerFor('o')){
+        warning= 'O "o" venceu'
+        playing= false
+    }else if(isFull()){
+        warning = 'Deu empate'
+        playing= false
+    }
+}
+
+function checkWinnerFor(player){
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+    
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1'
+    
+    ]
+    for(let w in pos){
+        let pArray = pos[w].split(',')
+        let hasWon = pArray.every( option =>square[option] === player)
+        if(hasWon){
+            return true
+        }
+    }
+    return false
+}
+function isFull(){
+    for(let i in square){
+        if(square[i]=== ''){
+            return false
+        }
+    }
+    return true
 }
